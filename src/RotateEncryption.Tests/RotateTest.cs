8f1715a28f34d4a2fc6275c458ed41df50c9101d
Somnuk Wongkhan <somnuk.wk@gmail.com>
2018-01-17 20:45:30 +0700

    Refactor

--- src/RotateEncryption.Tests/RotateTest.cs
+++ src/RotateEncryption.Tests/RotateTest.cs
@@ -8,6 +8,7 @@ namespace RotateEncryption.Tests {
             var input = "SecretToken1234";
             var encrypt = RotateEncryption.Rotate.Encrypt(input);
             var decrypt = RotateEncryption.Rotate.Decrypt(encrypt);
+            Console.WriteLine("{0} {1} {2}", input, encrypt, decrypt);
             Assert.Equal(input, decrypt);
         }
     }
--- src/RotateEncryption/Rotate.cs
+++ src/RotateEncryption/Rotate.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace RotateEncryption {
+    static internal class Extensions {
+        public static string ToStrings(this IEnumerable<char> values) =>
+            new String(values.ToArray());
+    }
+
     public class Rotate {
         static char[] characters = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".Reverse().ToArray();
-        static char[] rotatedCharacters = new char[0];
-
-        static Rotate() {
-            rotatedCharacters = RotateChars(characters);
-        }
+        static char[] rotatedCharacters = RotateChars(characters);
 
         private static char[] RotateChars(char[] data, int length = 100) {
             IEnumerable<T> cycle<T>(IEnumerable<T> input) {
@@ -24,18 +25,12 @@ namespace RotateEncryption {
             return rotate(data, length).ToArray();
         }
 
-        public static string Encrypt(string values) {
-            var indexs = values.ToArray().Select(x => Array.IndexOf(characters, x));
-            var rotatedChars = indexs.Select(x => rotatedCharacters.ElementAt(x)).ToArray();
-            var result = new String(rotatedChars);
-            return result;
-        }
+        public static string Encrypt(string values) =>
+            values.AsEnumerable().Select(x => Array.IndexOf(characters, x))
+                .Select(x => rotatedCharacters.ElementAt(x)).ToStrings();
 
-        public static string Decrypt(string values) {
-            var indexs = values.ToArray().Select(x => Array.IndexOf(rotatedCharacters, x));
-            var rotatedChars = indexs.Select(x => characters.ElementAt(x)).ToArray();
-            var result = new String(rotatedChars);
-            return result;
-        }
+        public static string Decrypt(string values) =>
+            values.AsEnumerable().Select(x => Array.IndexOf(rotatedCharacters, x))
+                .Select(x => characters.ElementAt(x)).ToStrings();
     }
 }
